a5e2f565d12f6a4cc35d620e3d6756cee25c62e4
YELIMINETI SHYAM REDDY <yshyam.reddy09@gmail.com>
2017-05-04 23:37:55 +0530

    mr codes for analysis on H1-B

--- Map-Reduce/Que9.java
+++ Map-Reduce/Que9.java
@@ -10,45 +10,44 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-//3)Which industry has the most number of Data Scientist positions?
+//9) Which are top ten employers who have the highest success rate in petitions?
+		
+public class Que9 {
 
-public class Que3 {
-
-	public static class dataMapp extends Mapper<LongWritable, Text, Text, Text> {
+	public static class map_grw extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
 		public void map(LongWritable key, Text value, Context con)
 				throws IOException, InterruptedException {
 			String[] str = value.toString().split("\t");
-
-			String job_title = str[4];
-			String petition = str[3];
-			if (job_title.equals("DATA SCIENTIST")) {
-				con.write(new Text(petition), new Text(job_title));
+			String employer_name = str[2];
+			String application = (str[6]);
+			if (str[1].equals("CERTIFIED") || str[1].equals("CERTIFIED-WITHDRWAN")) {
+				con.write(new Text(employer_name), new IntWritable(1));
 			}
 		}
 	}
 
-	public static class pet_Reducer extends
-			Reducer<Text, Text, NullWritable, Text> {
+	public static class reduce_grwt extends
+
+	Reducer<Text, IntWritable, NullWritable, Text> {
 		private TreeMap<Long, Text> repToRecordMap = new TreeMap<Long, Text>();
 
-		public void reduce(Text key, Iterable<Text> values, Context context)
-				throws IOException, InterruptedException {
-
-			int count = 0;
-			for (Text val : values) {
-				count++;
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum = sum + val.get();
 			}
-
-			String mytotal = String.format("%d", count);
+			String mytotal = String.format("%d", sum);
 			String myValue = key.toString();
 			myValue = myValue + ',' + mytotal;
 
-			repToRecordMap.put(new Long(count), new Text(myValue));
-			if (repToRecordMap.size() > 5) {
+			repToRecordMap.put(new Long(sum), new Text(myValue));
+			if (repToRecordMap.size() > 10) {
 				repToRecordMap.remove(repToRecordMap.firstKey());
 			}
 		}
@@ -65,14 +64,15 @@ public class Que3 {
 			ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "");
-		job.setJarByClass(Que3.class);
-		job.setMapperClass(dataMapp.class);
+		job.setJarByClass(Que9.class);
+		job.setMapperClass(map_grw.class);
 
-		job.setReducerClass(pet_Reducer.class);
+		job.setReducerClass(reduce_grwt.class);
+
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
 		job.setOutputKeyClass(NullWritable.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
